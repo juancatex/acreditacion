36b67ad7dbade5e63d1ada37aa043b6fc0c5b0b1
Ing. juan carlos <juan_carlos1345@hotmail.com>
2022-05-30 18:57:25 -0400

    pl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>ASCINALSS</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
  
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -103,7 +104,9 @@ label{
 }
 
        </style>
+       <script>window.Laravel = {csrfToken: '{{ csrf_token() }}'}</script>
 <script src="{{ asset('js/app.js') }}" defer></script>
+
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
